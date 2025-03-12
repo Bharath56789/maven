49651b6e17a5da7fc9e29683c9918af1a89cab84
BHARATH T M <BHARATH T M@LAPTOP-KR6AR41B>
2025-03-12 11:03:12 +0530

    selenium code

--- maven/src/main/java/com/easytrip/pom/HomePage.java
+++ maven/src/main/java/com/easytrip/pom/HomePage.java
@@ -1,5 +1,16 @@
 package com.easytrip.pom;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class HomePage {
+	@Test
+	public void add() {
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		System.out.println(driver.getTitle());
+		driver.close();
+	}
 
 }
